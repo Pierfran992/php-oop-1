ad68b7065ebae4f07befe8f28184dda5038eacd0
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-24 15:40:00 +0100

    Stato sviluppo: - Creazione metodo all'interno della classe Movie per stampare gli elementi in pagina; - Creazione dei generi da inserire nei film;

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ class Type {
 
     public $type;
 
+    // definisco il costruttore della classe
     public function __construct($type){
 
         $this -> type = $type;
@@ -22,6 +23,7 @@ class Movie {
     public $time;
     public $vote;
 
+    // definisco il costruttore della classe
     public function __construct($title, Type $type, $description, $time, $vote)
     {
         $this -> title = $title;
@@ -29,5 +31,26 @@ class Movie {
         $this -> description = $description;
         $this -> time = $time;
         $this -> vote = $vote;
+
+    }
+
+    // definisco il metodo per stampare a schermo i dati
+    public function getHMTL() {
+
+        return "<h2>" . "Titolo: " . $this->title . "</h2>"
+            . "<span>" . $this->type . "</span>"
+            . "<p>" . $this->description . "</p>"
+            . "<span>" . $this->time . "</span>" . "<br>"
+            . "<span>" . $this->vote . "</span>";
     }
 }
+
+// creo i generi da inserire nei relativi film
+$type1 = new Type ("Fantasy");
+$type2 = new Type ("Fantascienza");
+var_dump($type1);
+echo "<br><br>";
+var_dump($type2);
+
+
+// creo il film da stampare successivamente in pagina
