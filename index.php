257ea64060a2ea458ef449e39b621ccb306e4c9f
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-24 16:14:35 +0100

    Sviluppo: - Creazione funzione getType all'interno della classe Type; - Stampati in pagina attraverso il metodo getHTML i movie; Fix: -Rimossi i Vari var_dump; -Coretta la function getHTML richiamando al suo interno la function getType;

--- index.php
+++ index.php
@@ -11,6 +11,10 @@ class Type {
         $this -> type = $type;
     }
 
+    public function getType() {
+        return "<span>" . $this->type . "</span>";
+    }
+
 }
 
 // definizione classe Movie
@@ -38,7 +42,7 @@ class Movie {
     public function getHMTL() {
 
         return "<h2>" . "Titolo: " . $this->title . "</h2>"
-            . "<span>" . $this->type . "</span>"
+            . "<span>Genere: " . $this -> type -> getType()
             . "<p>" . $this->description . "</p>"
             . "<span>" . $this->time . "</span>" . "<br>"
             . "<span>" . $this->vote . "</span>";
@@ -48,23 +52,16 @@ class Movie {
 // creo i generi da inserire nei relativi film
 $type1 = new Type ("Fantasy");
 $type2 = new Type ("Fantascienza");
-var_dump($type1);
-echo "<br><br>";
-
-var_dump($type2);
-echo "<br><br>";
 
 // creo il film da stampare successivamente in pagina
 $movie1 = new Movie("Il Signore degli Anelli - La Compagnia dell'anello", $type1, "Un Anello per domarli, un Anello per trovarli,
 un Anello per ghermirli e nel buio incatenarli.", "178 min", "4.7");
-var_dump($movie1);
-
-echo "<br><br>";
 
 $movie2 = new Movie("Dune", $type2, "La pellicola è la prima parte dell'adattamento cinematografico del romanzo omonimo scritto da Frank Herbert, primo capitolo del ciclo di Dune,[3] già trasposto nel film del 1984 di David Lynch e con le miniserie televisive Dune - Il destino dell'universo (2000) e I figli di Dune (2003). ", "155 min", "4.0");
-var_dump($movie2);
 
+// stampo i film in pagina
+echo "<h1>Elenco Film</h1>";
 
-
-
-
+echo $movie1 -> getHMTL();
+echo "<br><br>";
+echo $movie2 -> getHMTL();
